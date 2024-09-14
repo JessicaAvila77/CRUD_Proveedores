805e989d6be3597ce79906901887c216ba01bf76
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-14 14:19:11 -0600

    Creando Modelo

---
+++ Models/Proveedor.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Proveedores.Models
+{
+    public class Proveedor
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        [NotNull]
+        public string Email { get; set; }
+
+        [NotNull]
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+    }
+}
