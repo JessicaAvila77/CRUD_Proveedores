329d8e7051a075ffdf90c552113404a99028303e
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-14 16:15:34 -0600

    Creando vista main (listado)

---
+++ Views/ProveedorMain.xaml.cs
@@ -0,0 +1,25 @@
+using AndroidX.Lifecycle;
+using CRUD_Proveedores.ViewModels;
+
+namespace CRUD_Proveedores.Views;
+
+public partial class ProveedorMain : ContentPage
+{
+	private ProveedorMainViewModel ViewModel;
+
+	public ProveedorMain()
+	{
+		InitializeComponent();
+		ViewModel = new ProveedorMainViewModel();
+		this.BindingContext = ViewModel;
+
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ViewModel.GetAll();
+    }
+
+
+}
