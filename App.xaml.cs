1f6222059f6d2917558301ac62e22d9aa1df9f84
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-14 17:52:44 -0600

    Corrigiendo nombre

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CRUD_Proveedores
+﻿using CRUD_Proveedores.Views;
+
+namespace CRUD_Proveedores
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new ProveedorMain());
         }
     }
 }
