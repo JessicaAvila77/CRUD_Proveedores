4454757133ef4c4c563620445b065783634f231e
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-14 15:38:18 -0600

    Creando vistas

---
+++ Views/AddProveedorForm.xaml.cs
@@ -0,0 +1,28 @@
+using CRUD_Proveedores.Models;
+using CRUD_Proveedores.ViewModels;
+using Javax.Security.Auth;
+
+namespace CRUD_Proveedores.Views;
+
+public partial class AddProveedorForm : ContentPage
+{
+    private AddProveedorFormViewModel ViewModel;
+
+    public AddProveedorForm()
+	{
+        InitializeComponent();
+        ViewModel = new AddProveedorFormViewModel();
+        BindingContext = ViewModel;
+
+     }
+
+    public AddProveedorForm(Proveedor proveedor)
+    {
+        InitializeComponent();
+        ViewModel = new AddProveedorFormViewModel(proveedor);
+        BindingContext = ViewModel;
+
+    }
+
+
+}
